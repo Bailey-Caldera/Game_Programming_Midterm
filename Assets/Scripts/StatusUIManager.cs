4c509f7ce5c2a2b676e8c26db0153879b78a4832
DominicA0 <dominic.arrezola6059@coyote.csusb.edu>
2026-03-17 15:02:07 -0700

    Update StatusUIManager.cs
    added cooldown timer

--- Assets/Scripts/StatusUIManager.cs
+++ Assets/Scripts/StatusUIManager.cs
@@ -5,11 +5,15 @@ public class StatusUIManager : MonoBehaviour
 {
     public TMP_Text waveText;
     public TMP_Text healthText;
+    public TMP_Text cooldownText;
+    //allows script to see cooldown status
+    public PlayerShooting playerShooting;
     
     private void Start()
     {
         waveText.text = $"Wave: 1";
         healthText.text = $"Health: 5";
+        cooldownText.text = "Cooldown: Fire";
     }
 
     private void OnEnable()
@@ -24,6 +28,27 @@ public class StatusUIManager : MonoBehaviour
         PlayerHealth.onPlayerDamage -= UpdatePlayerHealth;
         PlayerHealth.onHealthPackApplied -= UpdatePlayerHealth;
         LevelController.onNextWave -= UpdateWave;
+    }
+
+    private void Update()
+    {
+        //if player not shooting using right-click function stops run
+        if (playerShooting == null)
+        {
+            return;
+        }
+
+        //if cooldown relevant display time
+        if (playerShooting.Cooldown.IsCoolingDown)
+        {
+            cooldownText.text = $"Cooldown: {playerShooting.Cooldown.TimeR:F1}s";
+        }
+
+        //if cooldown is done display 0
+        else
+        {
+            cooldownText.text = "Cooldown: Fire";
+        }
     }
 
     private void UpdatePlayerHealth(int currentHP)
